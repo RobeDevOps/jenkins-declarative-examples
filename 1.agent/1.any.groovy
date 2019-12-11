c451e089e1a68722a9683d9d0234112178bf9c2f
robedevops <roberto.cardenas.isla@gmail.com>
2019-12-10 21:21:47 -0500

    using multiple stages with agent any

--- 1.agent/1.any.groovy
+++ 1.agent/1.any.groovy
@@ -6,5 +6,15 @@ pipeline {
                 echo 'Building...'
             }
         }
+        stage('Test') {
+            steps {
+                echo 'Testing...'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying...'
+            }
+        }
     }
 }
