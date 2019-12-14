7c4471f107c6705723a8d11e556595358896adec
robedevops <roberto.cardenas.isla@gmail.com>
2019-12-13 22:49:48 -0500

    using multiple docker configuration

--- 1.agent/3.dockerAgent.groovy
+++ 1.agent/3.dockerAgent.groovy
@@ -1,9 +1,16 @@
 pipeline {
-   agent { docker 'maven:3-alpine' } 
+    agent none 
     stages{
-        stage("Build"){
+        stage("Build for 7"){
+            agent { docker 'openjdk:7' }
             steps {
-                sh "mvn --version"
+                sh "java -version"
+            }
+        }
+        stage("Build for 8"){
+            agent { docker 'openjdk:8' }
+            steps {
+                sh "java -version"
             }
         }
     }
