7c26ef864d364e5f9414510015626e6ef88ba6e7
robedevops <roberto.cardenas.isla@gmail.com>
2019-12-16 20:44:26 -0500

    update sleep retry and timeout

--- 4.options/4.Timeout.groovy
+++ 4.options/4.Timeout.groovy
@@ -3,12 +3,13 @@ pipeline {
     options {
         // unit options
         // https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/TimeUnit.html
-        timeout(time: 1, unit: 'HOURS') 
+        timeout(time: 5, unit: 'SECONDS') 
     }
     stages {
         stage('Build') {
             steps {
                 echo 'Build...'
+                sleep 7
             }
         }
     }
